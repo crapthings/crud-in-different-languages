bb59a8f5f569b24d576ab317b845c71fb1de95f0
crapthings <crapthings@gmail.com>
2021-11-06 01:55:28 +0800

    update perl

--- perl-with-mojolicious/index.pl
+++ perl-with-mojolicious/index.pl
@@ -1,5 +1,21 @@
-use Mojolicious::Lite;
+use Mojolicious::Lite -signatures;
+use Mojo::JSON qw(decode_json encode_json to_json);
 
-get '/' => {text => 'I ♥ Mojolicious!'};
+our @users = ();
+# push @users, { fullname => 'blah blah blah' } for 1..10;
+
+# map {print} @users;
+
+get '/' => {
+  text => 'perl with mojolicious'
+};
+
+# get '/api/users' => {
+#   json => @users
+# };
+
+# put '/test' => sub ($c) {
+#   $c->render(json => $chars);
+# };
 
 app->start;
